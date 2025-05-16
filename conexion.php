0f28ba57aebe2fbe8ce852083a183290da8397e0
genesis <isic22.gzapatag@itesco.edu.mx>
2025-05-15 22:49:00 -0600

    yastoiharta

--- conexion.php
+++ conexion.php
@@ -3,15 +3,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    // En tu computadora local con XAMPP
     $host = "localhost";
     $usuario = "root";
-    $clave = "1234";
+    $clave = "";
     $base_de_datos = "IziFinanzas";
 } else {
-    $host = "auth-db1957.hstgr.io";
-    $usuario = "u143755789_root";
-    $clave = "Contraizi1234!";
-    $base_de_datos = "u143755789_IziFinanzas";
+    // En el servidor Hostinger
+    $host = "localhost"; // Usa localhost aquí, no auth-db...
+    $usuario = "u143755789_root"; // Verifica en tu panel de Hostinger
+    $clave = "Contraizi1234!";     // O la que hayas puesto
+    $base_de_datos = "u143755789_IziFinanzas"; // Nombre exacto
 }
 $conexion = new mysqli($host, $usuario, $clave, $base_de_datos);
 
